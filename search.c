fc9a8004274d8c78c758e8ab39ada81a142f18ce
tanushka-jha23 <tanushkajha670@gmail.com>
2024-02-22 23:50:21 +0530

    added binary search

--- search.c
+++ search.c
@@ -11,3 +11,25 @@ int linear_search(int* p, int v){
         }
     }
 }
+
+//binary search O[log n]
+int binary_search(int*p, int a, int b, int v){
+    if(b - a == 1){
+        if(get(p, a) == v){
+            return a;
+        }
+        return -1;
+    }
+    int c = (a + b) / 2;
+    if(get(p, c) == v){
+        return c;
+    }
+    else if(get(p, c) > v){
+        binary_search(p, a, c, v);
+    }
+    else{
+        binary_search(p, c, b, v);
+    }
+}
+
+
