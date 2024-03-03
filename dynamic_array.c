df1869277a444ef2d7f63d9669a4b313345b747a
tanushka-jha23 <tanushkajha670@gmail.com>
2024-03-03 16:25:21 +0530

    updated push with double pointer

--- dynamic_array.c
+++ dynamic_array.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* dynamic_array(){
+int** dynamic_array(){
+    int** t = (int**)malloc(sizeof(int*));
     int* p = (int*)malloc(7 * sizeof(int));
     *p = 5;
     *(p + 1) = -1;
@@ -11,43 +12,44 @@ int* dynamic_array(){
 
         i = i + 1;
     }
-    return p;
+    *t = p;
+    return t;
 }
 
-int get(int* p, int i){
-    return *(p + i + 2);
+int get(int** p, int i){
+    return *(*(p) + i + 2);
 }
 
-int* push(int* p, int v){
-    *(p + 1) = *(p + 1) + 1;
+int push(int** p, int v){
+    *(*(p) + 1) = *(*(p) + 1) + 1;
     int* q;
-    if(*(p + 1) >= *p){
-        int* q = (int*)malloc(((*p) * 2 + 2) * sizeof(int));
-        *q = (*p) * 2;
-        *(q + 1) = *(p + 1);
+    if(*(*(p) + 1) >= **p){
+        int* q = (int*)malloc((*(*p) * 2 + 2) * sizeof(int));
+        *q = *(*p) * 2;
+        *(q + 1) = *(*(p) + 1);
 
         int i = 0;
-        while(i < (*(q + 1))){
+        while(i < *(q + 1)){
             *(q + i + 2) = get(p, i);
 
             i = i + 1;
         }
         *(q + i + 2) = v;
 
-        return q;
+        *p = q;
     }
     else{
-        *(p + *(p + 1) + 2) = v;
+        *(*(p) + *(*(p) + 1) + 2) = v;
 
-        return p;
     }
+    return 0;
 }
 
-int printArray(int* a) {
+int printArray(int** a) {
     int i;
     printf("[ ");
-    for(i = 0; i < *(a + 1) + 1; i++) {
-        if(i == *(a + 1)){
+    for(i = 0; i <= *(*(a) + 1) ; i++) {
+        if(i == *(*(a) + 1)){
             printf("%d", get(a, i));
         }
         else{
@@ -56,4 +58,15 @@ int printArray(int* a) {
     }
     printf("]\n");
     return 0;
+}
+
+int main(){
+    int** a = dynamic_array();
+    push(a, 2);
+    push(a, -7);
+    push(a, 15);
+    push(a, 10);
+    push(a, -22);
+    push(a, 120);
+    printArray(a);
 }
