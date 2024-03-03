96e2316a50af014ccaec70b26e429f15af7f5d3b
tanushka-jha23 <tanushkajha670@gmail.com>
2024-03-03 14:38:03 +0530

    fixing error

--- dynamic_array.c
+++ dynamic_array.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* dynamic_array(int n){
-    int* p = malloc(n);
-    *p = n;
+int* dynamic_array(){
+    int* p = (int*)malloc(5 * sizeof(int));
+    *p = 5;
     *(p + 1) = -1;
     int i = 0;
     while(i < *p){
@@ -14,25 +14,31 @@ int* dynamic_array(int n){
     return p;
 }
 
-int getp(int* p, int i){
-    return(*(p + i + 2));
+int get(int* p, int i){
+    return *(p + i + 2);
 }
 
 int* push(int* p, int v){
     *(p + 1) = *(p + 1) + 1;
     int* q;
-    if(*(p + 1) > *p){
-        int* q = malloc((*p) * 2 + 2);
-        *q = *p * 2;
+    if(*(p + 1) >= *p){
+        int* q = (int*)malloc(((*p) * 2 + 2) * sizeof(int));
+        *q = (*p) * 2;
         *(q + 1) = *(p + 1);
 
         int i = 0;
-        while(i < *(q + 1) - 1){
-            *(q + i + 2) = getp(p, i);
+        while(i < (*(q + 1) - 1)){
+            *(q + i + 2) = get(p, i);
 
             i = i + 1;
         }
         *(q + i + 2) = v;
+    }
+    else{
+        int* q = malloc(((*p) + 2) * sizeof(int));
+        *q = *p;
+        *(q + 1) = *(p + 1);
+        *(q + *(p + 1) + 2) = v;
     }
 
     return q;
@@ -41,16 +47,15 @@ int* push(int* p, int v){
 int printArray(int* a) {
     int i;
     printf("[ ");
-    for(i = 0; i < *a; i++) {
-        
-        printf("%d, ", getp(a, i));
+    for(i = 0; i < *(a + 1); i++) {
+        printf("%d, ", get(a, i));
     }
     printf("]\n");
     return 0;
 }
 
 int main(){
-    int* a = dynamic_array(5);
-    push(a, 3);
-    printArray(a);
+    int* a = dynamic_array();
+    int* p = push(a, 3);
+    printArray(p);
 }
