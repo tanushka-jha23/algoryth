50a35f6df95bb843a932462d546dbcb365632d88
tanushka-jha23 <tanushkajha670@gmail.com>
2024-03-03 19:58:18 +0530

    added pop

--- dynamic_array.c
+++ dynamic_array.c
@@ -45,10 +45,41 @@ int push(int** p, int v){
     return 0;
 }
 
+int pop(int** p, int i){
+    *(*(p) + 1) = *(*(p) + 1) - 1;
+    int a = (*(*(p) + i + 2));
+
+    if((*(*(p) + 1) < (**p)/2)  &&  **p > 5){
+        int* q = (int*)malloc(((**p)/2) * sizeof(int));
+        *q = (**p)/2;
+        *(q + 1) = *(*(p) + 1);
+
+        int j = 0;
+        while(j < i){
+            *(q + j + 2) = get(p, j);
+
+            j = j + 1;
+        }
+        while(j <= *(q + 1)){
+            *(q + j + 2) = get(p, j + 1);
+
+            j = j + 1;
+        }
+        *p = q;
+    }
+    else{
+        int j;
+        for(j = i; j < *(*p + 1) + 1; j++){
+            *((*p) + j + 2) = get(p, j + 1);
+        }
+    }
+    return a;
+}
+
 int printArray(int** a) {
     int i;
     printf("[ ");
-    for(i = 0; i <= *(*(a) + 1) ; i++) {
+    for(i = 0; i < *(*(a) + 1) + 1 ; i++) {
         if(i == *(*(a) + 1)){
             printf("%d", get(a, i));
         }
@@ -61,12 +92,19 @@ int printArray(int** a) {
 }
 
 int main(){
-    int** a = dynamic_array();
-    push(a, 2);
-    push(a, -7);
-    push(a, 15);
+    int**a = dynamic_array();
+    push(a, 12);
     push(a, 10);
-    push(a, -22);
-    push(a, 120);
+    push(a, 40);
+    push(a, 53);
+    push(a, -10);
+    push(a, 0);
+    push(a, 17);
+
+
+    printArray(a);
+    pop(a, 1);
+    printArray(a);
+    pop(a, 3);
     printArray(a);
 }
