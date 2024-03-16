09208c181726f4160e193e0684787ae4af38be38
tanushka-jha23 <tanushkajha670@gmail.com>
2024-03-16 12:22:36 +0530

    implemented queues using linked list

--- Queues/queue.c
+++ Queues/queue.c
@@ -5,20 +5,20 @@
 typedef struct queue{
     Dynamic_array* Dynamic_array;
 }Queue;
-
+//constructor
 Queue* constructQueue(){
     Dynamic_array* d = createDynamicArray();
     Queue* a = (Queue*)malloc(sizeof(Queue));
     (*a).Dynamic_array = d;
     return a;
 }
-
+//enqueue(just like push in case of dynamic array)
 int enqueue(Queue* a, int v){
     push((*a).Dynamic_array, v);
 
     return 0;
 }
-
+//dequeue(remove the first value of queue)
 int dequeue(Queue* a){
     pop((*a).Dynamic_array, 0);
 
---
+++ Queues/queue2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "../Array and Linked list/linked_list.c"
+
+typedef struct queue{
+    LinkedList* list;
+}Queue2;
+
+Queue2* consQueue(){
+    LinkedList* l = createLinkedList();
+    Queue2* q = (*Queue2)malloc(sizeof(Queue2));
+    q->list = l;
+
+    return q;
+}
+
+int enqueue(Queue2* d, int v){
+    push(d->list, v);
+
+    return 0;
+}
+
+int dequeue(Queue2* d){
+    pop(d->list, 0);
+
+    return 0;
+}
+
+int main(){
+    Queue2* q = consQueue();
+    enqueue(q, 5);
+    enqueue(q, -10);
+    printList(q->list);
+    dequeue(q);
+    printList(q->list);
+
+    return 0;
+}
