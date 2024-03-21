35d81af09b897d8740ac248f56d1536def322047
tanushka-jha23 <tanushkajha670@gmail.com>
2024-03-21 21:07:14 +0530

    implemented priority queues

--- Queues/prique.c
+++ Queues/prique.c
@@ -14,37 +14,26 @@ Priq* createPq(){
     return q;
 }
 
-int swap(int a, int b){
-    int t = a;
-    a = b;
-    b = t;
-
-    return 0;
-}
-
-
 int enqueue(Priq* q, int v){
     push(q->array, v);
-    
-    if(q->array->fill != 0){
-        int k = q->array->fill + 1;
-        while(k > 0){
-            if(*(q->array->pointer + (k - 1)/ 2) < v){
-                swap(*(q->array->pointer + (k-1)/2), v);
-            }
-
-            k = (k - 1) / 2;
+    int k = q->array->fill;
+    while(k > 0){
+        if(*(q->array->pointer + (k - 1)/ 2) < v){
+            *(q->array->pointer) = *(q->array->pointer + (k - 1)/ 2);
+            *(q->array->pointer + (k - 1)/ 2) = v;
         }
+        k = (k - 1) / 2;
     }
     return 0;
 }
 
 int main(){
     Priq* p = createPq();
-    enqueue(p, 5);
-    enqueue(p, 9);
     enqueue(p, -1);
+    enqueue(p, 9);
+    enqueue(p, 5);
     enqueue(p, 0);
+    enqueue(p, -100);
     printArray(p->array);
     printf("\n");
 
